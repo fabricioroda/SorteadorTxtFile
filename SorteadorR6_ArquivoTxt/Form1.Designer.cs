300f6a4ac335fd639cd2ed46596d06ebc9226107
Fabricio Roda Sales <fsales@FAZENDA.MS>
2019-07-31 09:36:27 -0400

    Novo

---
+++ SorteadorR6_ArquivoTxt/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SorteadorR6_ArquivoTxt
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnSorteio = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(50, 27);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(145, 26);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Sorteio do R6";
+            // 
+            // btnSorteio
+            // 
+            this.btnSorteio.Location = new System.Drawing.Point(236, 21);
+            this.btnSorteio.Name = "btnSorteio";
+            this.btnSorteio.Size = new System.Drawing.Size(149, 43);
+            this.btnSorteio.TabIndex = 1;
+            this.btnSorteio.Text = "Sortear";
+            this.btnSorteio.UseVisualStyleBackColor = true;
+            this.btnSorteio.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Red;
+            this.lblResultado.Location = new System.Drawing.Point(50, 92);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(70, 26);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "label2";
+            this.lblResultado.Visible = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Enabled = false;
+            this.btnLimpar.Location = new System.Drawing.Point(391, 21);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(140, 43);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 156);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnSorteio);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmPrincipal";
+            this.Text = "Sorteio R6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnSorteio;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ SorteadorR6_ArquivoTxt/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SorteadorR6_ArquivoTxt
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            List<string> linhasL = new List<string>();
+            using (StreamReader reader = new StreamReader(@"C:\temp\R6.txt"))
+            {
+                string Linhas = string.Empty;
+                while ((Linhas = reader.ReadLine()) != null)
+                {
+                    linhasL.Add(Linhas);
+                }
+
+                Random rand = new Random();
+                int aleatorio = rand.Next(0, linhasL.Count);
+                lblResultado.Text = "O Sorteado foi " + (linhasL[aleatorio]);
+                Thread.Sleep(1500);
+                lblResultado.Visible = true;
+                btnSorteio.Enabled = false;
+                btnLimpar.Enabled = true;
+            }
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            btnSorteio.Enabled = true;
+            lblResultado.Text = "";
+            lblResultado.Visible = false;
+        }
+    }
+}
